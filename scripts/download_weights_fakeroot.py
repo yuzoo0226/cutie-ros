cf10efabd51ce0e0be4c521f06981135433eded5
yuzoo0226 <fryuzoo@gmail.com>
2025-06-27 21:46:10 +0900

    initalize models

---
+++ scripts/download_weights_fakeroot.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import cv2
+import rospy
+import torch
+import logging
+import threading
+import numpy as np
+from cv_bridge import CvBridge
+from PIL import Image as PILImage
+from torchvision.transforms.functional import to_tensor
+
+from cutie.inference.inference_core import InferenceCore
+from cutie.utils.get_default_model import get_default_model
+
+if __name__ == "__main__":
+    cutie = get_default_model()
+    processor = InferenceCore(cutie, cfg=cutie.cfg)
+    print("initalize complete!")
