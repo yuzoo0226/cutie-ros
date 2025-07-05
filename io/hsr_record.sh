13edf7996182e251e3c2f1cb465af0206a8bef71
yuzoo0226 <fryuzoo@gmail.com>
2025-07-06 04:51:28 +0900

    mask作成のためのrosbag作成コマンドをshell化

---
+++ io/hsr_record.sh
@@ -0,0 +1,17 @@
+rosbag record \
+    /hsrb/hand_camera/camera_info \
+    /hsrb/hand_camera/hand_camera_nodelet_manager/bond \
+    /hsrb/hand_camera/image_raw \
+    /hsrb/hand_camera/image_raw/compressed \
+    /hsrb/hand_camera/image_raw/compressedDepth \
+    /hsrb/head_rgbd_sensor/depth_registered/camera_info \
+    /hsrb/head_rgbd_sensor/depth_registered/image \
+    /hsrb/head_rgbd_sensor/depth_registered/image/compressed \
+    /hsrb/head_rgbd_sensor/depth_registered/image/compressedDepth \
+    /hsrb/head_rgbd_sensor/rgb/image_color \
+    /hsrb/head_rgbd_sensor/rgb/image_color/compressed \
+    /hsrb/head_rgbd_sensor/rgb/image_color/compressedDepth \
+    /hsrb/head_rgbd_sensor/rgb/image_raw \
+    /hsrb/head_rgbd_sensor/rgb/image_raw/compressed \
+    /hsrb/head_rgbd_sensor/rgb/image_raw/compressedDepth \
+    /hsrb/head_rgbd_sensor/rgb/image_raw/compressedDepth/parameter_descriptions
